e634b2fe558a59ecb3e588e2077d05f95c58f26e
babi2707 <90732683+babi2707@users.noreply.github.com>
2024-05-31 12:36:05 -0300

    firebase init

---
+++ firebase.js
@@ -0,0 +1,49 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  onSnapshot,
+  getDocs,
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signOut,
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyChTHnKxfDhnX0gXVStoNfvuZW0ucuW1F8",
+  authDomain: "bancoimobiliario-d2474.firebaseapp.com",
+  projectId: "bancoimobiliario-d2474",
+  storageBucket: "bancoimobiliario-d2474.appspot.com",
+  messagingSenderId: "574096287477",
+  appId: "1:574096287477:web:ed64975edbe220995747bc",
+  measurementId: "G-23Y42F0K1K",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+const auth = getAuth();
+
+export {
+  db,
+  collection,
+  addDoc,
+  onSnapshot,
+  getDocs,
+  auth,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signOut,
+};
